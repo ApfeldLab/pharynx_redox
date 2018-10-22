9455f5edb500eba5d292a413041b4a28a63a5aa3
Sean Johnsen <sean.b.johnsen@gmail.com>
2018-10-22 16:06:43 -0400

    Changed profile interpolation

--- matlab/sean/FullPharynxPipeline/measureIntensityAlongMidlines.m
+++ matlab/sean/FullPharynxPipeline/measureIntensityAlongMidlines.m
@@ -1,11 +1,13 @@
 function intensityProfiles = measureIntensityAlongMidlines(fluorescenceStack, midlines)
     nAnimals = size(fluorescenceStack, 3);
     imWidth = size(fluorescenceStack, 2);
-    intensityProfiles = zeros(imWidth, nAnimals);
+    intensityProfiles = zeros(imWidth * 10, nAnimals);
     xs = 1:imWidth;
     
     for i=1:nAnimals
-        intensityProfiles(:, i) = improfile(fluorescenceStack(:,:,i), xs, feval(midlines{i}, xs), imWidth);
+        ys = feval(midlines{i}, xs);
+        intensityProfiles(:, i) = improfile(fluorescenceStack(:,:,i), xs, ys, imWidth * 10, 'bilinear');
     end
+    intensityProfiles(isnan(intensityProfiles)) = 0;
 %     intensityProfiles = ssquare(clip_sj(intensityProfiles, threshold));
 end
--- matlab/sean/FullPharynxPipeline/trimProfile.m
+++ matlab/sean/FullPharynxPipeline/trimProfile.m
@@ -1,12 +1,14 @@
 function trimmed = trimProfile(intensityProfile)
     trimmed = intensityProfile;
+    trimmed(isnan(trimmed))=0;
     for i = 1:size(intensityProfile, 2)
         prof_i = intensityProfile(:,i);
 
         smoothed = smoothdata(prof_i, 'gaussian', 15);
-        left = find(abs(diff(smoothed)) > 300, 1);
-        right = find(abs(diff(smoothed)) > 200, 1, 'last');
-
+%         left = find(abs(diff(smoothed)) > 300, 1);
+%         right = find(abs(diff(smoothed)) > 200, 1, 'last');
+        left = find(smoothed > 2000, 1, 'first');
+        right = find(smoothed > 2000, 1, 'last');
         prof_i([1:left right:end]) = 0;
         trimmed(:,i) = prof_i;
     end
