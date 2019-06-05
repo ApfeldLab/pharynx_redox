d8ae6d9aa168cbd8869579c23ca92b4a1222abaa
Sean Johnsen <sean.b.johnsen@gmail.com>
2019-06-05 14:06:04 -0400

    took AP alignment out of rotation function, added cropping

--- matlab/sean/core/rotatePharynx.m
+++ matlab/sean/core/rotatePharynx.m
@@ -1,9 +1,27 @@
-function rotated = rotatePharynx(imStack, segStack)
+function [rotated_FL, rotated_seg] = rotatePharynx(imStack, segStack, cropDims)
     [centroids, orientations] = calcCentroidsAndOrientations(segStack);
-    rotated = imStack;
+    rotated_FL = imStack;
+    rotated_seg = segStack;
     center = size(imStack(:,:,1).') / 2;
+    textprogressbar('Translating / Rotating: ');
     for i=1:size(imStack, 3)
-        translated = imtranslate(rotated(:,:,i), [center(1) - centroids(i, 1) center(2) - centroids(i, 2)]);
-        rotated(:,:,i) = imrotate(translated, -orientations(i), 'bilinear', 'crop');
+        textprogressbar(100 * i / size(imStack, 3));
+        translated_FL = imtranslate(rotated_FL(:,:,i), [center(1) - centroids(i, 1) center(2) - centroids(i, 2)]);
+        rotated_FL(:,:,i) = imrotate(translated_FL, -orientations(i), 'bilinear', 'crop');
+        
+        translated_seg = imtranslate(rotated_seg(:,:,i), [center(1) - centroids(i, 1) center(2) - centroids(i, 2)]);
+        rotated_seg(:,:,i) = logical(uint8(floor(imrotate(translated_seg, -orientations(i), 'nearest', 'crop'))));
     end
+    
+    if cropDims % [height width]
+        B = center(2)-cropDims(1)/2;
+        T = center(2)+cropDims(1)/2;
+        L = center(1)-cropDims(2)/2;
+        R = center(1)+cropDims(2)/2;
+        
+        rotated_FL = rotated_FL(B:T,L:R,:);
+        rotated_seg = rotated_seg(B:T,L:R,:);
+    end
+    
+    textprogressbar(' done');
 end
