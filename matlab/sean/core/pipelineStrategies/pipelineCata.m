fa12d9226664d01e9e96308dce2f6cfd9bbcb009
Sean Johnsen <sean.b.johnsen@gmail.com>
2019-06-14 11:22:38 -0400

    Changed some function signatures & new synth. data
    synthetic dataset is translational

--- matlab/sean/core/pipelineStrategies/pipelineCata.m
+++ matlab/sean/core/pipelineStrategies/pipelineCata.m
@@ -9,14 +9,21 @@ imFL2 = subtractMedians(imFL2);
 % Segment
 seg = segmentPharynx(imFL1, 0, 2000);
 
+% Mask
+imFL1 = imFL1 .* seg;
+imFL2 = imFL2 .* seg;
+
+% Rotate
+[rot_FL1, ~] = rotatePharynx(imFL1, seg);
+[rot_FL2, rot_seg] = rotatePharynx(imFL2, seg);
+
 % Draw Midlines
-midlines = calculateMidlines(imTL, seg, imFL1, 0);
+% midlines = calculateMidlines(imTL, seg, imFL1, 0);
+midlines = calculateMidlinesNoTL(rot_seg);
 
 % Measure Under Midlines
 N_DATA_POINTS = 1000;
 
-
-i1 = measureIntensityCata(imFL1, seg, midlines, N_DATA_POINTS);
-i2 = measureIntensityCata(imFL2, seg, midlines, N_DATA_POINTS);
+i1 = measureIntensityCata(rot_FL1, rot_seg, midlines, N_DATA_POINTS);
+i2 = measureIntensityCata(rot_FL2, rot_seg, midlines, N_DATA_POINTS);
 end
-
--- matlab/sean/core/pipelineStrategies/pipelineTwoMidlinesTwoMasks.m
+++ matlab/sean/core/pipelineStrategies/pipelineTwoMidlinesTwoMasks.m
@@ -9,10 +9,18 @@ imFL2 = subtractMedians(imFL2);
 seg1 = segmentPharynx(imFL1, 0);
 seg2 = segmentPharynx(imFL2, 0);
 
+% Rotate
+[rot_imFL1, rot_seg1] = rotatePharynx(imFL1, seg1);
+[rot_imFL2, rot_seg2] = rotatePharynx(imFL2, seg2);
+
 % Draw Midlines
 disp('Calculating Midlines... Please hold');
-midlines1 = calculateMidlines(imTL, seg1, imFL1, 0);
-midlines2 = calculateMidlines(imTL, seg2, imFL2, 0);
+% midlines1 = calculateMidlines(imTL, seg1, imFL1, 0);
+% midlines2 = calculateMidlines(imTL, seg2, imFL2, 0);
+
+midlines1 = calculateMidlinesNoTL(rot_seg1);
+% midlines2 = calculateMidlinesNoTL(rot_seg2);
+midlines2 = midlines1;
 disp('Done calculating midlines');
 
 % Measure Under Midlines
@@ -21,7 +29,6 @@ N_DATA_POINTS = 1000;
 lrBounds1 = double(getLeftRightBounds(seg1));
 lrBounds2 = double(getLeftRightBounds(seg2));
 
-[i1, ~, ~, i1_raw] = measureAndTrim(imFL1, midlines1, lrBounds1, N_DATA_POINTS);
-[i2, ~, ~, i2_raw] = measureAndTrim(imFL2, midlines2, lrBounds2, N_DATA_POINTS);
+[i1, i1_raw] = measureAndTrim(rot_imFL1, midlines1, 2000, N_DATA_POINTS);
+[i2, i2_raw] = measureAndTrim(rot_imFL2, midlines2, 2000, N_DATA_POINTS);
 end
-
