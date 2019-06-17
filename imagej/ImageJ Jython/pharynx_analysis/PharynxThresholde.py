9db26ba6ec89535838d53ca62f4f5e4429106b5c
Sean Johnsen <sean.b.johnsen@gmail.com>
2019-06-17 09:17:32 -0400

    moved python and imagej stuff around
    Former-commit-id: b6d5c44ebcca38298b07a76e40e689b54865dfbe

---
+++ imagej/ImageJ Jython/pharynx_analysis/PharynxThresholde.py
@@ -0,0 +1,18 @@
+from ij.plugin.filter import ExtendedPluginFilter
+
+
+class PharynxThresholder(ExtendedPluginFilter):
+	def setup(arg, imp):
+		pass
+
+	def showDialog(imp, command, pfr):
+		pass
+
+	def setNPasses(nPasses):
+		pass
+
+	def run(ip):
+		pass
+
+	def setup("final", imp):
+		pass
