74e782ec03b2afa492ad6b60b3a22ab3e79bc73c
Sean Johnsen <sean.b.johnsen@gmail.com>
2019-06-24 20:06:07 -0700

    deleted old python things

--- python/results_server/results_server/results/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Experiment(models.Model):
-    date = models.DateField('experiment date')
-    data = models.BinaryField()
-    experimenter = models.CharField(max_length=100)
