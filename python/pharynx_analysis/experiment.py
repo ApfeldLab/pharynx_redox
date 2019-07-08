633d7b2dc7a7ef06d54c0c16e8bb3e74c8c87324
Sean Johnsen <sean.b.johnsen@gmail.com>
2019-07-08 13:15:45 -0400

    corrected layout for ratio images

--- python/pharynx_analysis/experiment.py
+++ python/pharynx_analysis/experiment.py
@@ -7,6 +7,14 @@ from pharynx_analysis import pharynx_io as pio
 
 class Experiment:
 
+    regions = {
+        'pm7': [.07, .13],
+        'pm6': [.15, .21],
+        'pm5': [.25, .47],
+        'pm4': [.53, .64],
+        'pm3': [.68, .93],
+    }
+
     def __init__(self, raw_image_path: str):
         self.raw_image_path = raw_image_path
 
@@ -17,42 +25,42 @@ def get_non_tl(data_array):
 
 class PairExperiment(Experiment):
 
-    def __init__(self, raw_image_path: str, imaging_scheme: str, strain_map: [str]):
+    midline_map = {
+        '410_1': '410_1',
+        '470_1': '470_1',
+        '410_2': '410_2',
+        '470_2': '470_2',
+    }
+
+    wavelengths = list(midline_map.keys())
+
+    image_display_order = [
+        '410_1', '470_1', 'r1',
+        '410_2', '470_2', 'r2'
+    ]
+
+    trimmed_profile_length = 100
+    n_midline_pts = 200
+    seg_threshold = 2000
+
+    def __init__(self, raw_image_path: str, imaging_scheme: str, strain_map: [str], midline_smoothing=1e8):
         super().__init__(raw_image_path)
         self.imaging_scheme = imaging_scheme
         self.strain_map = strain_map
         self.raw_image_data = pio.load_images(self.raw_image_path, imaging_scheme, strain_map)
         self.fl_images = self.raw_image_data.where(self.raw_image_data.wavelength != 'TL', drop=True)
 
-        self.trimmed_profile_length = 100
-        self.n_midline_pts = 200
-        self.seg_threshold = 2000
         self.seg_stack = ip.segment_pharynxes(get_non_tl(self.raw_image_data))
-
-        self.midline_map = {
-            '410_1': '410_1',
-            '470_1': '470_1',
-            '410_2': '410_2',
-            '470_2': '470_2',
-        }
-
-        self.regions = {
-            'pm7': [.07, .13],
-            'pm6': [.15, .21],
-            'pm5': [.25, .47],
-            'pm4': [.53, .64],
-            'pm3': [.68, .93],
-        }
-
-        self.wavelengths = list(self.midline_map.keys())
 
         self.rot_fl = []
         self.rot_seg = []
 
         # Do the rotations
+        self.rotation_wavelength = '410_1'
+
         for wvl in self.midline_map.keys():
             rot_fl, rot_seg = ip.center_and_rotate_pharynxes(get_non_tl(self.raw_image_data).sel(wavelength=wvl),
-                                                             self.seg_stack.sel(wavelength=wvl))
+                                                             self.seg_stack.sel(wavelength=self.rotation_wavelength))
             # crop_height=30, crop_width=70)
             self.rot_fl.append(rot_fl)
             self.rot_seg.append(rot_seg)
@@ -60,10 +68,15 @@ class PairExperiment(Experiment):
         self.rot_fl = xr.concat(self.rot_fl, dim='wavelength')
         self.rot_seg = xr.concat(self.rot_seg, dim='wavelength')
 
+        rot_r1 = self.rot_fl.sel(wavelength='410_1') / self.rot_fl.sel(wavelength='470_1')
+        rot_r2 = self.rot_fl.sel(wavelength='410_2') / self.rot_fl.sel(wavelength='470_2')
+
+        self.rot_ratios = xr.concat([rot_r1, rot_r2], 'pair')
+
         # Calculate midlines
         self.midlines = {}
         for wvl in self.midline_map.keys():
-            self.midlines[wvl] = ip.calculate_midlines(self.rot_seg.sel(wavelength=wvl))
+            self.midlines[wvl] = ip.calculate_midlines(self.rot_seg.sel(wavelength=wvl), s=midline_smoothing)
 
         # Measure under midlines
         # TODO broken when cropped
