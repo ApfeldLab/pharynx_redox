ac46a52948ba9f66dd9e435baa06ddbae69a4fbd
Sean Johnsen <sean.b.johnsen@gmail.com>
2019-07-10 16:09:38 -0400

    back strain map GUI with pandas

--- python/gui/widgets.py
+++ python/gui/widgets.py
@@ -173,14 +173,18 @@ class PandasModel(QtCore.QAbstractTableModel):
         self._data = data
 
     def rowCount(self, parent=None):
-        return self._data.shape[0]
+        return len(self._data.values)
 
     def columnCount(self, parent=None):
-        return self._data.shape[1]
+        return self._data.columns.size
 
     def data(self, index, role=QtCore.Qt.DisplayRole):
         if index.isValid():
             if role == QtCore.Qt.DisplayRole:
-                return str(self._data.iloc[index.row(), index.column()])
+                return str(self._data.values[index.row()][index.column()])
         return None
 
+    def headerData(self, col, orientation, role):
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+            return self._data.columns[col]
+        return None
