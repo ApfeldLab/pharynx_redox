99bb6f3b714e1a6ffae79a21eb26e5871d4ef05b
Sean Johnsen <sean.b.johnsen@gmail.com>
2019-09-05 12:14:44 -0400

    remove MATLAB files

--- matlab/core/functional_registration/smoothRoughRegisterDiscretize.m
+++
@@ -1,9 +0,0 @@
-function [i1_reg,i2_reg] = smoothRoughRegisterDiscretize(i1, i2, smoothLambda, roughLambda, warpLambda, nSamples)
-%SMOOTHROUGHREGISTERDISCRETIZE Summary of this function goes here
-%   Detailed explanation goes here
-[roughFD1, regRoughFD2] = smoothRoughRegister(i1, i2, smoothLambda, roughLambda, warpLambda);
-
-xs = linspace(1,100,nSamples);
-i1_reg = eval_fd(xs, roughFD1);
-i2_reg = eval_fd(xs, regRoughFD2);
-end
--- matlab/core/misc/genRandInRange.m
+++
@@ -1,4 +0,0 @@
-function r = genRandInRange(min_,max_,n)
-%GENRANDINRANGE Generate n random numbers in range [min_ max].
-    r = (max_-min_).*rand(n,1) + min_;
-end
