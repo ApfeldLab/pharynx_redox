9689dc339912d9542f4abbc804553a689b28b0ed
Sean Johnsen <sean.b.johnsen@gmail.com>
2020-02-19 17:01:31 -0500

    add points with double-click; somtimes buggy?
    Former-commit-id: 6e7165a12399a424c692f8cf1fa6821f445bcad2

--- pharynx_redox/gui/qt_spline_editor.py
+++ pharynx_redox/gui/qt_spline_editor.py
@@ -5,45 +5,46 @@ import pyqtgraph as pg
 from pyqtgraph import Point
 from pyqtgraph.graphicsItems.ROI import ROI
 from scipy import interpolate
+from scipy.spatial.distance import cdist
 from dataclasses import dataclass, astuple
 from pharynx_redox.gui.qt_py_files.spline_editor import Ui_Form
 import numpy as np
 
 
 class Spline:
-    def __init__(self, ctrl_pts=[], n_points=100):
+    def __init__(self, ctrl_pts=[], n_points=500):
         """
         Initialize the Spline object
         
         Parameters
         ----------
         ctrl_pts : list, optional
-            a , by default []
+            the list of points that this spline will interpolate between, by default []
         n_points : int, optional
             the number of points under which to evaluate the spline, by default 100
         """
         self._spl = None
-
-        self._xs = np.linspace(0, 1, n_points)
         self.ctrl_pts = []
 
         self.add_ctrl_pts(ctrl_pts)
-        self.update_spline()
 
     def add_ctrl_pts(self, ctrl_pts):
         for (x, y) in ctrl_pts:
             self.ctrl_pts.append(Point(x, y))
-
-    def update_spline(self):
-        xys = np.asarray(list(map(list, self.ctrl_pts)))
-        if len(xys) > 0:
-            self._spl = interpolate.CubicSpline(np.linspace(0, 1, len(xys)), xys)
+        self._update_spline()
 
     def set_ctrl_pts(self, pos):
-        self.ctrl_pts = pos
-        self.update_spline()
+        self.ctrl_pts = []
+        self.add_ctrl_pts(pos)
 
-    def __call__(self, xs=None):
+    def _update_spline(self):
+        xys = np.asarray(list(map(list, self.ctrl_pts)))
+        if len(xys) > 0:
+            self._spl = interpolate.Akima1DInterpolator(
+                np.linspace(0, 1, len(xys)), xys
+            )
+
+    def __call__(self, n=500):
         """
         evaluate the spline
         
@@ -59,142 +60,21 @@ class Spline:
                 M[:, 0] = x
                 M[:, 1] = y
         """
-        if xs is None:
-            return self._spl(self._xs)
-        else:
-            return self._spl(xs)
+        xs = np.linspace(0, 1, n)
+        return self._spl(xs)
 
 
-class SplineROI(pg.GraphItem):
-    def __init__(self, *args, **kwargs):
-        self.dragPoint = None
-        self.dragOffset = None
+class SplineROI(pg.GraphicsObject):
+
+    sigClicked = QtCore.Signal(object)
+
+    def __init__(self, pos, *args, pen="default", **kwargs):
         super(SplineROI, self).__init__(*args, **kwargs)
 
-    def setData(self, **kwds):
-        self.data = kwds
-        if "pos" in self.data:
-            npts = self.data["pos"].shape[0]
-            self.data["adj"] = np.column_stack(
-                (np.arange(0, npts - 1), np.arange(1, npts))
-            )
-            self.data["data"] = np.empty(npts, dtype=[("index", int)])
-            self.data["data"]["index"] = np.arange(npts)
-        self.updateGraph()
-
-    def updateGraph(self):
-        pg.GraphItem.setData(self, **self.data)
-
-    def mouseDragEvent(self, ev):
-        if ev.button() != QtCore.Qt.LeftButton:
-            ev.ignore()
-            return
-
-        if ev.isStart():
-            pos = ev.buttonDownPos()
-            pts = self.scatter.pointsAt(pos)
-            if len(pts) == 0:
-                ev.ignore()
-                return
-            self.dragPoint = pts[0]
-            ind = pts[0].data()[0]
-            self.dragOffset = self.data["pos"][ind][1] - pos[1]
-        elif ev.isFinish():
-            self.dragPoint = None
-            return
-        else:
-            if self.dragPoint is None:
-                ev.ignore()
-                return
-
-        ind = self.dragPoint.data()[0]
-        self.data["pos"][ind][1] = ev.pos()[1] + self.dragOffset
-        self.updateGraph()
-        ev.accept()
-
-
-class Graph(pg.GraphItem):
-    def __init__(self):
         self.dragPoint = None
         self.dragOffset = None
-        self.textItems = []
-        self.crv = pg.PlotCurveItem()
-        pg.GraphItem.__init__(self)
-        self.scatter.sigClicked.connect(self.clicked)
 
-    def setData(self, **kwds):
-        self.text = kwds.pop("text", [])
-        self.data = kwds
-        if "pos" in self.data:
-            npts = self.data["pos"].shape[0]
-            self.data["data"] = np.empty(npts, dtype=[("index", int)])
-            self.data["data"]["index"] = np.arange(npts)
-        self.setTexts(self.text)
-        self.updateGraph()
-
-    def setTexts(self, text):
-        for i in self.textItems:
-            i.scene().removeItem(i)
-        self.textItems = []
-        for t in text:
-            item = pg.TextItem(t)
-            self.textItems.append(item)
-            item.setParentItem(self)
-
-    def updateGraph(self):
-        if "pos" in self.data:
-            print(self.data["pos"])
-            crv_data = Spline(ctrl_pts=self.data["pos"])()
-            self.crv.setData(pos=crv_data)
-        pg.GraphItem.setData(self, **self.data)
-        for i, item in enumerate(self.textItems):
-            item.setPos(*self.data["pos"][i])
-
-    def mouseDragEvent(self, ev):
-        if ev.button() != QtCore.Qt.LeftButton:
-            ev.ignore()
-            return
-
-        if ev.isStart():
-            # We are already one step into the drag.
-            # Find the point(s) at the mouse cursor when the button was first
-            # pressed:
-            pos = ev.buttonDownPos()
-            pts = self.scatter.pointsAt(pos)
-            if len(pts) == 0:
-                ev.ignore()
-                return
-            self.dragPoint = pts[0]
-            ind = pts[0].data()[0]
-            self.dragOffset = self.data["pos"][ind] - pos
-        elif ev.isFinish():
-            self.dragPoint = None
-            return
-        else:
-            if self.dragPoint is None:
-                ev.ignore()
-                return
-
-        ind = self.dragPoint.data()[0]
-        self.data["pos"][ind] = ev.pos() + self.dragOffset
-        self.setData(pos=self.pos)
-        ev.accept()
-
-    def clicked(self, pts):
-        print("clicked: %s" % pts)
-
-    def paint(self, p, *args):
-        p.setPen(self.currentPen)
-
-        # super().paint(p, *args)
-
-
-class OldSplineROI(pg.GraphicsObject):
-    def __init__(self, pos, *args, pen="default", **kwargs):
-        super(OldSplineROI, self).__init__(*args, **kwargs)
-
-        self.dragPoint = None
-        self.dragOffset = None
+        self.ctrl_pts = pos
 
         self.spl = Spline(ctrl_pts=pos)
 
@@ -204,24 +84,56 @@ class OldSplineROI(pg.GraphicsObject):
 
         self.picture = None
         self.pen = pen
-        self.setData(pos=pos)
+        self.setData(ctrl_pts=pos)
+
+        self._mouseShape = None
 
     def _update(self):
         self.picture = None
         self.prepareGeometryChange()
+        self.mouseShape()
         self.update()
 
-    def setData(self, **kwds):
-        self.data = kwds
-        # if "pos" in self.data:
-        # npts = len(self.data["pos"])
-        # self.data["index"] = np.arange(npts)
-
-        self._update()
-        self.scatter.setData(**kwds)
-        self.spl.set_ctrl_pts(pos=self.data["pos"])
+    def setData(self, ctrl_pts):
+        self.ctrl_pts = ctrl_pts
+        self.scatter.setData(pos=ctrl_pts)
+        self.spl.set_ctrl_pts(pos=ctrl_pts)
         self.informViewBoundsChanged()
         self._update()
+
+    def mouseDoubleClickEvent(self, ev):
+        if self.mouseShape().contains(ev.pos()):
+            ev.accept()
+            self.sigClicked.emit(self)
+
+            click_pos = np.asarray([ev.pos().x(), ev.pos().y()])
+            crv_data = self.spl()
+
+            ctrl_pts_passed = 0
+            epsilon = 0.1
+
+            # make a copy so we can mutate it without losing ctrl pts in the app
+            _ctrl_pts = self.ctrl_pts.copy()
+
+            # We find the closest point on the curve to where we clicked
+            closest_crv_pt_idx = np.argmin(cdist([click_pos,], crv_data))
+
+            # and move along the curve until we get to that point
+            # all the while, we are interested in how many control points we pass
+            for crv_pt in crv_data[:closest_crv_pt_idx, :]:
+                ctrl_pt = _ctrl_pts[0]
+                if epsilon > cdist([crv_pt,], [ctrl_pt])[0]:
+                    # we have reached a control point
+                    ctrl_pts_passed += 1
+
+                    # remove that ctrl point from future considerations (because there
+                    # are likely many curve points that are close to the curve point we
+                    # just passed)
+                    _ctrl_pts.pop(0)
+
+            print(f"inserting at {ctrl_pts_passed}")
+            self.ctrl_pts.insert(ctrl_pts_passed, tuple(click_pos))
+            self.setData(self.ctrl_pts)
 
     def mouseDragEvent(self, ev):
         if ev.button() != QtCore.Qt.LeftButton:
@@ -239,7 +151,7 @@ class OldSplineROI(pg.GraphicsObject):
                 return
             self.dragPoint = pts[0]
             ind = pts[0]._index
-            self.dragOffset = self.data["pos"][ind] - pos
+            self.dragOffset = self.ctrl_pts[ind] - pos
         elif ev.isFinish():
             self.dragPoint = None
             return
@@ -248,14 +160,14 @@ class OldSplineROI(pg.GraphicsObject):
                 ev.ignore()
                 return
 
-        # ind = self.dragPoint.data()[0]
         ind = self.dragPoint._index
-        self.data["pos"][ind] = ev.pos() + self.dragOffset
-        self.setData(pos=self.data["pos"])
+        self.ctrl_pts[ind] = tuple(ev.pos() + self.dragOffset)
+        self.setData(ctrl_pts=self.ctrl_pts)
         ev.accept()
 
     def clicked(self, pts):
         print("clicked: %s" % pts)
+        print(f"index: {self.scatter.pointsAt(pts.pos())[0]._index}")
 
     def generatePicture(self):
         self.picture = QtGui.QPicture()
@@ -310,11 +222,33 @@ class OldSplineROI(pg.GraphicsObject):
         )
 
     def dataBounds(self, *args, **kwds):
-        # return pg.PlotCurveItem(pos=self.data["pos"]).dataBounds(*args, **kwds)
+        # return pg.PlotCurveItem(pos=self.pos).dataBounds(*args, **kwds)
         return self.scatter.dataBounds(*args, **kwds)
 
     def pixelPadding(self):
         return self.scatter.pixelPadding()
+
+    def getPath(self):
+        crv_data = self.spl()
+        x, y = crv_data[:, 0], crv_data[:, 1]
+        return pg.functions.arrayToQPath(x, y)
+
+    def mouseShape(self):
+        """
+        Return a QPainterPath representing the clickable shape of the curve
+
+        """
+        # if self._mouseShape is None:
+        view = self.getViewBox()
+        if view is None:
+            return QtGui.QPainterPath()
+        stroker = QtGui.QPainterPathStroker()
+        path = self.getPath()
+        path = self.mapToItem(view, path)
+        stroker.setWidth(20)
+        mousePath = stroker.createStroke(path)
+        self._mouseShape = self.mapFromItem(view, mousePath)
+        return self._mouseShape
 
 
 class SplineEditorWidget(QtGui.QWidget):
@@ -323,10 +257,7 @@ class SplineEditorWidget(QtGui.QWidget):
     def __init__(self):
         super(SplineEditorWidget, self).__init__()
 
-        # self.spl = Spline()
-        self.spl_roi = OldSplineROI(pos=[(10, 10), (0, 0), (1, 2)])
-        # self.spl_roi = Graph()
-        # self.spl_roi.setData(pos=np.asarray([(10, 10), (0, 0), (1, 2)]))
+        self.spl_roi = SplineROI(pos=[(0, 0), (1, 1), (10, 10), (10, 20)])
 
         self.setGeometry(300, 300, 450, 450)
         self.setWindowTitle("Bezier Curves")
@@ -387,7 +318,6 @@ class SplineEditorWidget(QtGui.QWidget):
 
 if __name__ == "__main__":
     qapp = QtWidgets.QApplication(sys.argv)
-    # s = SplineROI([(0, 1), (1, 2)])
     se = SplineEditorWidget()
     se.show()
     qapp.exec_()
