39c7f210dd6f25ddbb84ec653a830711daa27ca1
Sean <sean.b.johnsen@gmail.com>
2020-05-18 17:02:53 -0500

    make  script

--- pharedox/scripts/__main__.py
+++ pharedox/scripts/__main__.py
@@ -22,13 +22,24 @@ def cli(debug):
 
 
 @cli.command()
-@click.argument("directory", type=click.Path(exists=True))
-@click.option("--gui/--command-line", default=False)
-def analyze(directory, gui):
+@click.argument(
+    "dir", type=click.Path(exists=True),
+)
+@click.option(
+    "--gui/--command-line",
+    default=False,
+    help="use a graphical user interface, or run the whole pipeline without intervention on the command-line",
+)
+@click.option(
+    "--tissue",
+    type=click.Choice(["pharynx", "gut", "neuron"], case_sensitive=False),
+    help="the tissue to analyze",
+)
+def analyze(dir, gui, tissue):
     """Analyze an experiment"""
     from pharedox import experiment
 
-    exp = experiment.Experiment(Path(directory))
+    exp = experiment.Experiment(Path(dir))
     if gui:
         from pharedox.gui.gui_napari import App
 
@@ -39,7 +50,36 @@ def analyze(directory, gui):
 
 
 @cli.command()
-@click.option("--output", "-o", default=None, type=click.Path(exists=False))
+@click.option(
+    "--z",
+    default="animal",
+    type=str,
+    help="dimension to place on the Z-axis. All other dimensions will be split into different stacks.",
+)
+@click.option(
+    "--output",
+    "-o",
+    default=None,
+    type=click.Path(exists=False),
+    help="directory to put output tiffs into, defaults to current directory",
+)
 @click.argument("filepath", type=click.Path(exists=True))
 def split_nc(filepath):
+    """Split an xarray DataArray into multiple Tiffs"""
     raise NotImplementedError
+
+
+@cli.command()
+def create_settings():
+    """Create a settings file using the default template and place in current directory"""
+    import os
+    import shutil
+
+    path_to_example_settings = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+        "defaule-settings.yaml",
+    )
+    path_to_new_settings = os.path.join(os.getcwd(), "settings.yaml")
+
+    shutil.copyfile(path_to_example_settings, path_to_new_settings)
+    print(f"Generated settings file at {path_to_new_settings}")
