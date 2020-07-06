9906ca21b196921015361923267144ae8514c33d
Sean <sean.b.johnsen@gmail.com>
2020-07-06 13:22:48 -0400

    add `plot_multiple_pop_wvl` to the plots

--- pharedox/plots.py
+++ pharedox/plots.py
@@ -917,3 +917,65 @@ def plot_multiple_pop_errors(
     ax.legend(loc="lower left")
 
     return fig, ax
+
+
+def plot_multiple_pop_wvl(
+    data_and_labels,
+    wvl="r",
+    ylim=None,
+    xlim=None,
+    add_regions=True,
+    figsize=(20, 10),
+    dpi=100,
+    regions=constants.untrimmed_regions,
+):
+    """Plot multiple error profiles and their corresponding labels
+
+    Parameters
+    ----------
+    data_and_labels : List[Tuple(xr.DataArray, str)]
+        A list of (data, label), one for each data set to plot
+    ylim : Tuple(float, float), optional
+        The y limits of the plot, by default None
+    xlim : Tuple(float, float), optional
+        The x-limits of the plot, by default None
+    add_regions : bool, optional
+        Whether or not to add regions to the plot, by default True
+    figsize : Tuple(float, floa), optional
+        The figure size, by default (20, 10)
+    dpi : int, optional
+        The DPI of the plot, by default 100
+    regions : dict, optional
+        The regions to plot, if enabled, by default constants.untrimmed_regions
+
+    Returns
+    -------
+    fig, ax
+        The figure and axis object
+    """
+
+    fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
+
+    for data, label in data_and_labels:
+        xs = np.linspace(0, 1, data.position.size)
+        plot_profile_avg_with_bounds(
+            data.sel(timepoint=0, wavelength=wvl).mean(dim="pair"),
+            xs=xs,
+            ax=ax,
+            label=label,
+        )
+
+    if ylim:
+        ax.set_ylim(*ylim)
+    if xlim:
+        ax.set_xlim(*xlim)
+
+    if add_regions:
+        add_regions_to_axis(ax, regions, alpha=0.3, hide_labels=True)
+
+    ax.set_ylabel(wvl)
+    ax.set_xlabel("position along midline")
+
+    ax.legend(loc="lower left")
+
+    return fig, ax
